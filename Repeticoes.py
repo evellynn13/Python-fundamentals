df82d89a2930dda37f122aa32331ae6f7e35ebb3
Evellyn Santos <rayssaevellyn2005@gmail.com>
2026-08-30 16:53:05 -0300

    Add loop to count from 0 to 12

---
+++ Repeticoes.py
@@ -0,0 +1,7 @@
+# Declaracao de uma variavel contador 
+contador = 0
+# Uso da variavel em um loop para contar ate 13
+while contador < 13:
+    print("Contador é:", contador)
+    #Incrementando o valor do contador
+    contador += 1
