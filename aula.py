fbbcac5573832b3e1436001a0b0dbcabf71093c9
Evellyn <rayssaevellyn2005@gmail.com>
2026-08-30 15:34:58 -0300

    Adiciona fundamentos de Python

---
+++ aula.py
@@ -0,0 +1,41 @@
+import math
+
+# calculando a raiz quadrada de um numero
+numero = 16
+raiz_quadrada = math.sqrt(numero)
+print(f"A raiz quadrada de {numero} é {raiz_quadrada}") 
+
+   # Entrada de dados
+nota1 = 8
+nota2 = 7
+# Calculando a média
+media = (nota1 + nota2) / 2
+# imprimindo resultado
+print("A média das notas é:", media)
+
+# Entrada de dados
+nota1 = 8
+nota2 = 9
+nota3 = 10
+nota4 = 5
+# Calculando a média
+media = (nota1 + nota2 + nota3 + nota4) / 4
+# imprimindo resultado
+print("A média das notas é:", media)
+
+ # Declarando de variáveis para
+ # armazenar as dimensoes do retângulo
+largura = 8
+altura = 5
+# Calculo da area usando a formula: area = largura * altura
+area = largura * altura
+# imprimindo resultado
+print("A área do retângulo é:", area)
+
+# Declaracao de uma variavel contador 
+contador = 0
+# Uso da variavel em um loop para contar ate 13
+while contador < 13:
+    print("Contador é:", contador)
+    #Incrementando o valor do contador
+    contador += 1
